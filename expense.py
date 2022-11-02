a512537b0b6e7ecf7ff5ae7355b07509d3d58c34
W-Johnson <william.johnson@epita.fr>
2022-11-02 21:44:49 +0100

    first part

--- expense.py
+++ expense.py
@@ -7,10 +7,20 @@ userList = [Separator('= Spender List =')]
 
 input_file = csv.DictReader(open("users.csv"))
 for row in input_file:
-   # print(row)
     userList.append(row)
 
-print(userList)
+# Checked the good user for checkbox
+
+
+def add_checked_user(list, user):
+
+    for spender in list[1:]:
+        if user == spender['name']:
+            list.remove(spender)
+            list.append({'name': spender['name'], 'checked': True})
+    return list
+
+
 expense_questions = [
     {
         "type": "input",
@@ -26,10 +36,17 @@ expense_questions = [
         'type': 'checkbox',
         'qmark': '💰',
         'message': 'Select users concerned by the expense',
-        'name': 'name',
+        'name': 'spender',
         'choices': userList,
-        'validate': lambda answer: 'You must choose at least one topping.'
-        if len(answer) == 0 else True
+        'validate': lambda answer: len(answer) == 1 or "Select only one user",
+    },
+    {
+        'type': 'checkbox',
+        'qmark': '💰',
+        'message': 'Select users concerned by the expense',
+        'name': 'otherSpenders',
+        'choices': lambda answer: add_checked_user(userList, answer['spender'][0])
+
     }
 
 ]
@@ -37,18 +54,17 @@ expense_questions = [
 
 def new_expense(*args):
     infos = prompt(expense_questions)
-    numberSpenders = len(infos['name'])
-    equallySpent = int(infos['amount']) / numberSpenders
+    # numberSpenders = len(infos['name'])
+    # equallySpent = int(infos['amount']) / numberSpenders
     # for name in infos['name']:
     #     print(name)
     #     print(equallySpent)
     # Writing the informations on external file
     with open('expense_report.csv', 'a', newline='') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=';',
-                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        for name in infos['name']:
-            spamwriter.writerow(
-                [equallySpent, infos['label'], name])
+                                quotechar=' ', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(
+            [infos['amount'], infos['label'], infos['spender'][0], ';'.join(infos['otherSpenders'])])
 
     print("Expense Added ! " + str(infos))
     return True
