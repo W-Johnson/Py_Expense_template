27699ecd32eb1b841fcaa6e8627b519d8992214f
W-Johnson <william.johnson@epita.fr>
2022-11-02 20:05:41 +0100

    write to csv file

--- expense.py
+++ expense.py
@@ -1,30 +1,34 @@
 from PyInquirer import prompt
+import csv
 
 expense_questions = [
     {
-        "type":"input",
-        "name":"amount",
-        "message":"New Expense - Amount: ",
+        "type": "input",
+        "name": "amount",
+        "message": "New Expense - Amount: ",
     },
     {
-        "type":"input",
-        "name":"label",
-        "message":"New Expense - Label: ",
+        "type": "input",
+        "name": "label",
+        "message": "New Expense - Label: ",
     },
     {
-        "type":"input",
-        "name":"spender",
-        "message":"New Expense - Spender: ",
+        "type": "input",
+        "name": "spender",
+        "message": "New Expense - Spender: ",
     },
 
 ]
 
 
-
 def new_expense(*args):
     infos = prompt(expense_questions)
     # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
-    print("Expense Added !")
+    with open('register.csv', 'a', newline='') as csvfile:
+
+        spamwriter = csv.writer(csvfile, delimiter=';',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(
+            [infos['amount'], infos['label'], infos['spender']])
+    print("Expense Added ! " + str(infos))
     return True
-
-
