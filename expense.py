64f8d786042528148c570ac6403dd66f91cc6155
W-Johnson <william.johnson@epita.fr>
2022-11-02 20:49:58 +0100

    shared expense with otheer users

--- expense.py
+++ expense.py
@@ -1,6 +1,16 @@
-from PyInquirer import prompt
+from PyInquirer import prompt, Separator
 import csv
 
+
+# Add user in the spender choice, the first line of the csv file must be equal to 'name' (without quotes)
+userList = [Separator('= Spender List =')]
+
+input_file = csv.DictReader(open("users.csv"))
+for row in input_file:
+   # print(row)
+    userList.append(row)
+
+print(userList)
 expense_questions = [
     {
         "type": "input",
@@ -13,22 +23,32 @@ expense_questions = [
         "message": "New Expense - Label: ",
     },
     {
-        "type": "input",
-        "name": "spender",
-        "message": "New Expense - Spender: ",
-    },
+        'type': 'checkbox',
+        'qmark': '💰',
+        'message': 'Select users concerned by the expense',
+        'name': 'name',
+        'choices': userList,
+        'validate': lambda answer: 'You must choose at least one topping.'
+        if len(answer) == 0 else True
+    }
 
 ]
 
 
 def new_expense(*args):
     infos = prompt(expense_questions)
-    # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
-    with open('register.csv', 'a', newline='') as csvfile:
-
+    numberSpenders = len(infos['name'])
+    equallySpent = int(infos['amount']) / numberSpenders
+    # for name in infos['name']:
+    #     print(name)
+    #     print(equallySpent)
+    # Writing the informations on external file
+    with open('expense_report.csv', 'a', newline='') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=';',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        spamwriter.writerow(
-            [infos['amount'], infos['label'], infos['spender']])
+        for name in infos['name']:
+            spamwriter.writerow(
+                [equallySpent, infos['label'], name])
+
     print("Expense Added ! " + str(infos))
     return True
--- user.py
+++ user.py
@@ -1,8 +1,25 @@
 from PyInquirer import prompt
+import csv
+
 user_questions = [
+    {
+        "type": "input",
+        "name": "name",
+        "message": "New user name: ",
+        # validate input must not be empty
+        "validate": lambda answer: len(answer) > 0 or "Please enter a name",
+    }
 
 ]
 
+
 def add_user():
-    # This function should create a new user, asking for its name
+    # this function add a new user
+    infos = prompt(user_questions)
+    with open('users.csv', 'a', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=';',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(
+            [infos['name']])
+    print("User " + str(infos['name']) + " Added ! ")
     return
